7749ea33f781ea7401cd1600a76dfc5893089c3f
karanghadage807-coder <karanghadage807@gmail.com>
2026-04-25 17:41:40 +0530

    feat: add About and Products pages with routing

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
               </li>
               <li class="nav-item">
                 <Link class="nav-link active" to="/product">
-                  Product
+                  Products
                 </Link>
               </li>
               <li class="nav-item">
