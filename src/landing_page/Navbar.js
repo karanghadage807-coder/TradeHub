0803523e948216892fb1b53f5ddf96f810662bb8
karanghadage807-coder <karanghadage807@gmail.com>
2026-04-25 11:43:19 +0530

    feat: configure app routing and add initial routes for pages

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 function Navbar() {
   return (
       <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#FFF"}}>
         <div class="container p-2">
-          <a class="navbar-brand" href="#">
+          <Link class="navbar-brand" to="/">
             <img src="media/images/logo.svg" style={{width:"25%"}} alt="Logo"></img>
-          </a>
+          </Link>
           <button
             class="navbar-toggler"
             type="button"
@@ -23,29 +24,29 @@ function Navbar() {
             <form class="d-flex" role="search">
             <ul class="navbar-nav mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+                <Link class="nav-link active" aria-current="page" to="/signup">
                   Signup
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <Link class="nav-link active" to="/about">
                   About
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <Link class="nav-link active" to="/product">
                   Product
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <Link class="nav-link active" to="/pricing">
                   Pricing
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <Link class="nav-link active" to="/support">
                   Support
-                </a>
+                </Link>
               </li>
             </ul>
             </form>
--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
+import Brokerage from "./Brokerage";
 
 function PricingPage() {
     return ( 
-        <h1>Pricing Page</h1>
+        <>
+        <Hero />
+        <OpenAccount />
+        <Brokerage />
+        </>
      );
 }
 
