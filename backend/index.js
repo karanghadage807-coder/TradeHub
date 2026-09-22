5db2248b2ff546694aa0041dcbeea61981e7141a
karanghadage807-coder <karanghadage807@gmail.com>
2026-09-23 00:04:41 +0530

    Update TradeHub trading dashboard

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel.js");
 const { PositionsModel } = require("./model/PositionsModel.js");
+const { OrdersModel } = require("./model/OrdersModel.js");
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
@@ -186,14 +187,27 @@ app.use(bodyParser.json());
   res.send("done");
 }); */
 
-app.get("/allHoldings",async (req,res)=>{
-    let allHoldings = await HoldingsModel.find({});
-    res.json(allHoldings);
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
 });
 
-app.get("/allPositions",async (req,res)=>{
-    let allPositions = await PositionsModel.find({});
-    res.json(allPositions);
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order Saved!");
 });
 
 app.listen(PORT, () => {
